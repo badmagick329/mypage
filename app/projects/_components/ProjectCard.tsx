f14071d23af3d2c80db8678b1dbdfcdf19068390
badmagick329 <uzairfarooqi@gmail.com>
2025-12-03 16:43:30 +0000

    feat: additional ui tweaking

--- app/projects/_components/ProjectCard.tsx
+++ app/projects/_components/ProjectCard.tsx
@@ -11,23 +11,28 @@ export default function ProjectCard({
   tech,
 }: ProjectData) {
   return (
-    <article className="bg-background-light shadow-card flex w-full max-w-md min-w-xs flex-col items-start gap-4 overflow-hidden rounded-md">
-      <Heading name={name} homePage={homePage} />
+    <article className="bg-background-light shadow-card @container flex w-full flex-col items-start gap-4 overflow-hidden rounded-md">
+      <section className="bg-background-lighter w-full py-4 text-center">
+        <Heading name={name} homePage={homePage} />
+      </section>
       <div className="flex flex-col gap-8 px-2">
-        <p className="xs:text-base text-foreground-muted text-sm">
+        <p className="text-foreground-muted text-sm @xs:text-base">
           {description}
         </p>
         <section className="flex flex-col gap-1">
-          <span className="xs:text-lg text-base font-bold">Why?</span>
-          <span className="xs:text-base text-sm">{why}</span>
+          <span className="text-base font-bold @xs:text-lg">Why?</span>
+          <span className="text-sm @xs:text-base">{why}</span>
         </section>
       </div>
       <footer className="flex grow flex-col justify-end gap-2 px-2 py-4">
         <section className="flex flex-col gap-1">
-          <h3 className="xs:text-xl text-base font-semibold">Tech</h3>
+          <h3 className="text-base font-semibold @xs:text-xl">Tech</h3>
           <ul className="flex flex-wrap gap-2">
             {tech.map((t) => (
-              <li className="xs:text-base text-sm" key={t}>
+              <li
+                className="bg-background-lighter text-foreground-strong rounded-md px-2 py-1 text-xs @xs:text-sm"
+                key={t}
+              >
                 {t}
               </li>
             ))}
@@ -36,11 +41,11 @@ export default function ProjectCard({
         <Link
           href={`https://github.com/badmagick329/${githubProjectName}`}
           target="_blank"
-          className="xs:text-sm text-foreground-muted text-xs hover:underline"
+          className="text-foreground-muted text-xs hover:underline @xs:text-sm"
         >
           <span className="flex gap-2">
             {`https://github.com/badmagick329/${githubProjectName}`}
-            <ExternalLink className="xs:h-2.5 xs:w-2.5 mt-1 h-1 w-1" />
+            <ExternalLink className="xs:w-2.5 mt-1 h-2 w-2 @xs:h-2.5" />
           </span>
         </Link>
       </footer>
@@ -50,19 +55,15 @@ export default function ProjectCard({
 
 function Heading({ name, homePage }: { name: string; homePage?: string }) {
   if (!homePage) {
-    return (
-      <h3 className="bg-background-lighter/60 xs:text-xl w-full py-4 text-center text-base font-semibold">
-        {name}
-      </h3>
-    );
+    return <h3 className="text-base font-semibold @xs:text-xl">{name}</h3>;
   }
 
   return (
-    <h3 className="bg-background-lighter/60 xs:text-xl w-full py-4 text-center text-base font-semibold">
+    <h3 className="text-foreground-strong text-base font-semibold @xs:text-xl">
       <Link href={homePage} target="_blank" className="hover:underline">
         <span className="flex justify-center gap-2">
           {name}
-          <ExternalLink className="xs:h-4 xs:w-4 mt-1 h-2.5 w-2.5" />
+          <ExternalLink className="xs:w-4 mt-1 h-2.5 w-2.5 @xs:h-4" />
         </span>
       </Link>
     </h3>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,10 +5,11 @@ export default function Projects() {
   return (
     <div className="flex w-full flex-col px-2">
       <h1 className="my-8 text-center text-3xl font-black">Projects</h1>
-      <div className="grid grid-cols-1 justify-items-center-safe gap-4 md:grid-cols-3">
+      <div className="grid-auto-fill justify-items-center-safe sm:px-2 md:px-8 xl:px-24">
         {projectsData.map((p) => (
           <ProjectCard
             key={p.name}
+            homePage={p?.homePage}
             name={p.name}
             githubProjectName={p.githubProjectName}
             description={p.description}
